dc3af722e688ed663e38b1aadd0d2256dbebf75a
Antonio Aquino <comercialfarepresentacoes@gmail.com>
2026-05-28 16:30:30 -0300

    feat(sprint2): harden domain validation layer

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from './domain/types.js';
 export * from './domain/stateMachine.js';
 export * from './domain/ownership.js';
 export * from './domain/reasons.js';
+export * from './domain/errors.js';
+export * from './domain/validation.js';
 export * from './domain/commercialDocument.js';
